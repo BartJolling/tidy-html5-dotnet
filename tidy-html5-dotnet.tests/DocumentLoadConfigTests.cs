fcf8c3e9cdf0a600daf4806bb0bd6306325ff9a2
Bart Jolling <BartJolling@users.noreply.github.com>
2024-10-16 09:46:57 +0200

    Load Config Test: assert on the loaded config options

--- tidy-html5-dotnet.tests/DocumentLoadConfigTests.cs
+++ tidy-html5-dotnet.tests/DocumentLoadConfigTests.cs
@@ -13,10 +13,14 @@ public class DocumentLoadConfigTests
         Assert.NotNull(tidyDocument);
 
         Assert.Equal(Encodings.Utf8, tidyDocument.EncodingOptions.CharacterEncoding);
+        Assert.Equal(AccessibilityCheckLevels.TidyClassic, tidyDocument.DiagnosticOptions.AccessibilityCheckLevel);
+        Assert.True(tidyDocument.DisplayOptions.ShowInfo);
 
         var configStatus = tidyDocument.LoadConfig(configFileName);
         Assert.Equal(DocumentStatuses.Success, configStatus);
 
         Assert.Equal(Encodings.Latin1, tidyDocument.EncodingOptions.CharacterEncoding);
+        Assert.Equal(AccessibilityCheckLevels.Priority1, tidyDocument.DiagnosticOptions.AccessibilityCheckLevel);
+        Assert.False(tidyDocument.DisplayOptions.ShowInfo);
     }
 }
